8e2d893f488e6b00d5c6faa35a170520827dee7d
moriarty39 <alissa.galat@gmail.com>
2020-04-06 03:50:52 -0400

    javascript one

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,55 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    "use strict";
+    const x = [];
+    let figure = Math.pow(b, 2) - 4 * a * c;
+    if (figure > 0) {
+        let squareRoot_1 = (- b + Math.sqrt(figure)) / (2 * a);
+        let squareRoot_2 = (- b - Math.sqrt(figure)) / (2 * a);
+        x.push(squareRoot_1, squareRoot_2);
+     } else if (figure == 0) {
+        let squareRoot_3 = - b / (2 * a);
+        x.push(squareRoot_3);
+     } else {
+        return x;
+     }
+
+    return x;  
+
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
-}
+function getAverageMark(marks = []){
+    let averageMark = 0;
+    sum = array => {
+       let sum = 0
+       array.forEach(function (value) {
+           sum += value;
+       });
+       return sum
+   }
+   if (marks.length == 0) {
+       return 0;
+   } else if (marks.length <= 5) {
+       averageMark = sum(marks) / marks.length
+   } else {
+       console.log("Total: " + marks.length);
+       let sliceArray = marks.slice(0, 5);
+       averageMark = sum(sliceArray) / sliceArray.length;
+   }
+   
+    return averageMark;
+ }
+    
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+    let today = new Date();
+        year = today.getFullYear();
+        yearOfBirth = dateOfBirthday.getFullYear();
+        yearsOld = year - yearOfBirth;
+        result = '';
+    if (yearsOld > 18) {
+        result = (`Не желаете ли олд-фэшн, ${name}?`);
+    } else {
+        result = (`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`);
+    }
+    return result;
 }
