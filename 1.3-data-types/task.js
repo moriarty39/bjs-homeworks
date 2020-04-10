02f6558fa9d0854ea467aa1b07da2247f5cd0de6
moriarty39 <alissa.galat@gmail.com>
2020-04-10 18:49:18 -0400

    java script additions

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,32 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let date1 = new Date;
+    date2 = date;
+    year1 = date1.getFullYear();
+    year2 = date2.getFullYear();
+    month1 = date1.getMonth();
+    month2 = date2.getMonth();
+    creditBody = amount - contribution;
+    fractionPercent = percent / 100 / 12;
+if(month1===0){ 
+    month1++;
+    month2++;
+}
+let perMonths; 
+perMonths = (year2 - year1) * 12 + (month2 - month1);
+
+let monthDue = creditBody * (fractionPercent + fractionPercent / (Math.pow((1 + fractionPercent), perMonths) - 1))
+let totalAmount = monthDue * perMonths;
+totalAmount = parseFloat(totalAmount.toFixed(2));
+console.log(perMonths);
+console.log(creditBody);
+console.log(monthDue);
+return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name == '' || name == 'null' || name =='undefined') {
+		name = 'Аноним'
+	}
+	
+	greeting = `Привет, мир! Меня зовут ${name}`;
 }
