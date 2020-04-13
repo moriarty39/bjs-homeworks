8b1088c9f7daf4d9da2f985af3de437b6586d525
moriarty39 <alissa.galat@gmail.com>
2020-04-13 01:12:32 -0400

    jssss

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -52,9 +52,9 @@ function getAverageScore(data) {
       count++;
       result.average = sumResult / count;
   }
-  // if(!result.length){
-  //   return result.average = 0;
-  // }
+   if(!result.length){
+     return result.average = 0;
+   }
    return result;
 }
 
