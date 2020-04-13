a954e253b4c0c2c24ec3b5a7a223a0ce049d2304
moriarty39 <alissa.galat@gmail.com>
2020-04-13 00:33:28 -0400

    js

---
+++ 2.1-functions/task.js
@@ -0,0 +1,35 @@
+"use strict";
+
+function getSolutions(a, b, c) {
+    const D = Math.pow(b, 2) - 4 * a * c;
+    const obj = {
+        D,
+        roots: []
+}
+    if (D > 0) {
+        let x1 = (- b + Math.sqrt(D)) / (2 * a);
+        let x2 = (- b - Math.sqrt(D)) / (2 * a);
+        obj.roots.push(x1, x2);
+     } else if (D == 0) {
+        let x1 = - b / (2 * a);
+        obj.roots.push(x1);
+     } 
+     
+    return obj;
+}
+
+function showSolutionsMessage(a, b, c) {
+    let result = getSolutions(a, b, c);
+        
+    console.log (`Вычисляем корни квадратного уравнения ${a} * x**2 + ${b} * x + ${c}`);
+    console.log(`Значение дискриминанта: ${result.D}`);
+    if (result.D > 0) {
+        console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`)
+     } else if (result.D == 0) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.roots}`)
+     } else {
+        console.log(`Уравнение не имеет вещественных корней`)
+     }
+     
+}
+showSolutionsMessage(7,20,-3)
