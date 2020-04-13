8a2d75fb2047e121de98c58aabf97fff3f311ffd
moriarty39 <alissa.galat@gmail.com>
2020-04-13 01:06:55 -0400

    js7

--- 2.1-functions/task.js
+++ 2.1-functions/task.js
@@ -1,41 +1,62 @@
-"use strict";
+"use strict"
 
-function getSolutions(a, b, c) {
-    const D = Math.pow(b, 2) - 4 * a * c;
-    const obj = {
-        D,
-        roots: []
+
+let D;
+
+function getSolutions( a, b, c ) { 
+    let roots = [];
+    
+    D = Math.pow(b,2) - 4 * a * c; 
+
+    if (D < 0) {
+        console.log(`D : ${D}, roots: ${roots} `);  
+        return {
+            discr : D,
+            root : roots
+        }
+
+    } else if (D == 0) {
+        let x1;
+        x1 = -b / (2 * a); 
+        roots[0] = x1;
+        console.log( `D : ${D}, roots: ${roots} `);
+        return {
+            discr : D,
+            root : roots
+        }
+    } else if (D > 0) {
+        let x1,x2;
+        x1 = (-b + Math.sqrt(D)) / (2 * a);  
+        x2 = (-b - Math.sqrt(D)) / (2 * a); 
+        roots[0] = x1;
+        roots[1] = x2;
+        console.log( `D : ${D}, roots: ${roots} `);
+        return {   
+            discr : D,
+            root : roots
+        }
+    }
 }
-    if (D > 0) {
-        let x1 = (- b + Math.sqrt(D)) / (2 * a);
-        let x2 = (- b - Math.sqrt(D)) / (2 * a);
-        obj.roots.push(x1, x2);
-     } else if (D == 0) {
-        let x1 = - b / (2 * a);
-        obj.roots.push(x1);
-     } 
-     
-    return obj;
+
+function showSolutionsMessage( a, b, c ) {
+    let result = getSolutions( a, b, c );
+    console.log(`Вычисляем корни квадратного уравнения ${a}x² + ${b}x + ${c}`);
+    console.log(`Значение дискриминанта: ${result.discr}`);
+    
+    if (result.discr < 0) {
+        console.log('Уравнение не имеет вещественных корней')
+    } else if (result.discr == 0) {
+        console.log(`Уравнение имеет один корень X₁ = ${result.root[0]}`);
+    } else if (result.discr > 0) {
+        console.log(`Уравнение имеет два корня. X₁ = ${result.root[0]}, X₂ = ${result.root[1]}`)
+    }
 }
 
-function showSolutionsMessage(a, b, c) {
-    let result = getSolutions(a, b, c);
-        
-    console.log (`Вычисляем корни квадратного уравнения ${a} * x**2 + ${b} * x + ${c}`);
-    console.log(`Значение дискриминанта: ${result.D}`);
-    if (result.D > 0) {
-        console.log(`Уравнение имеет два корня. X₁ = ${result.roots[0]}, X₂ = ${result.roots[1]}`)
-     } else if (result.D == 0) {
-        console.log(`Уравнение имеет один корень X₁ = ${result.roots}`)
-     } else {
-        console.log(`Уравнение не имеет вещественных корней`)
-     }
-     
-}
-showSolutionsMessage(7,20,-3)
+   showSolutionsMessage(2,4,2);
 
 
-const data = {
+
+let assessment = {
     algebra : [2,4,5,2,3,4],
     geometry : [2,4,5],
     russian : [3,3,4,5],
@@ -44,64 +65,68 @@ const data = {
     english : [4,4,3],
     poetry : [5,3,4],
     chemistry : [2],
-    french : []
+    french : [4,4]
 }
-  }
-  function getAverageScore(data) {
-    const result = {};
-    let sumResult = 0;
-    let count = 0;
-    const average = {};
-    for (let courses in data) {
-        result[courses] = getAverageMark(data[courses]);
-        sumResult += result[courses];
-        count++;
-        result.average = sumResult / count;
+
+let sum;  
+let sumMidAssesstment = 0;  
+
+function getAverageMark(marks) {   
+    for (let i = 0; i < marks.length; i++) {
+        sum+=marks[i];
     }
+    return sum/marks.length
+}
+
+
+function getAverageScore(data) {
+
+    let midAssessment = {    
+        _proto_ : data       
+    };  
+
+    let general = 0;         
     
-     return result;
-  }
-  
-  
-  function getAverageMark(grades){
-    let averageMark = 0;
-    const sum = array => {
-       let sum = 0
-       array.forEach(function (value) {
-           sum += value;
-       });
-       return sum
-   }
-   if (grades.length == 0) {
-       return 0;
-   } else {
-       averageMark = sum(grades) / grades.length
-   }
-    return averageMark;
-  }
-  
-  const result = getAverageScore(data);
-  console.log(result);
-  
-
-
-
-
-
-
-  
-
-  function getPersonData(secretData) {
-    let firstName = getDecodedValue(secretData.aaa);
-    let lastName = getDecodedValue(secretData.bbb);
-    return {firstName, lastName};
-  }
-  
-  function getDecodedValue(secret) {
-    if (secret == 1) {
-      return "Эмильо";
-    } else {
-      return "Родриго";
+    for (let key in data) {    /
+        sum = 0;
+        general = general + 1;    
+        let value = data [key];
+        midAssessment[key] = getAverageMark(value); 
+        sumMidAssesstment += midAssessment[key];    
     }
-  } 
-  console.log (getPersonData({aaa:0, bbb:0}))
+    midAssessment.average = sumMidAssesstment / general;  
+return (console.log(midAssessment));
+}
+
+getAverageScore(assessment);
+
+
+
+function getDecodedValue(secret) {    
+    
+    if (secret == 1) {
+        name = 'Эмильо';
+    } else if (secret == 0) {
+        name = "Родриго";
+    }
+    return name;
+   
+}
+
+let initCode = {    
+    aaa : 1,
+    bbb : 1
+}
+
+function getPersonData(secretData) {
+   console.log (secretData);
+   let finalCode = {
+       firstName : "",
+       lastName : ""
+   }
+   finalCode.firstName = getDecodedValue(secretData.aaa);
+   finalCode.lastName = getDecodedValue(secretData.bbb);
+   return console.log(finalCode); 
+}
+
+getPersonData(initCode);
