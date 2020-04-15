b437de2a19ae3ad354c7f508eef2deba8a5e0fc6
moriarty39 <alissa.galat@gmail.com>
2020-04-14 20:53:42 -0400

    js3

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -11,9 +11,17 @@ function getAnimalSound(animal) {
 }
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let average = 0;
+    let value = 0;
+    let roundedAverage;
+    for (let i=0; i< marks.length; i++) {   
+        value = Number(marks[i]) + value;    
+    }
+    average = value / marks.length;
+    roundedAverage = Math.round(average);
+    return roundedAverage
 }
+
 
 function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
