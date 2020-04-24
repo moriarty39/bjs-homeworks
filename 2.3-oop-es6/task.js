71fb1ca845015ba676b245286890be82df271c06
moriarty39 <alissa.galat@gmail.com>
2020-04-23 23:41:53 -0400

    js55

--- 2.3-oop-es6/task.js
+++ 2.3-oop-es6/task.js
@@ -70,7 +70,7 @@ const longBow = new Weapon({
     durability: bow.durability, 
     range: 4
 });
-const ax = new Weapon({
+const axe = new Weapon({
     name: 'Секира', 
     attack: 27, 
     durability: 800, 
@@ -84,30 +84,6 @@ const stormStaff = new Weapon({
 });
 
 
-// arm.takeDamage(20);
-// console.log(arm.durability); // Infinity
-
-// bow.takeDamage(20);
-// console.log(bow.durability); // 180
-
-// bow.takeDamage(200);
-// console.log(bow.durability); // 0
-
-
-// console.log(bow.durability); // 0
-// console.log(bow.getDamage()); // 0
-
-// console.log(arm.durability); // Infinity
-// console.log(arm.getDamage()); // 1
-
-
-// console.log(bow.durability); // 0
-// console.log(bow.isBroken()); // true
-
-// console.log(arm.durability); // Infinity
-// console.log(arm.isBroken()); // false
-
-// Задача 2
 
 class Arm extends Weapon{
     constructor() {
@@ -183,10 +159,10 @@ class StormStaff extends Staff{
 const stormStaff2 = new StormStaff();
 
 
-console.log(stormStaff2.name); // Посох Бури
-console.log(stormStaff2.attack); // 10
-console.log(stormStaff2.durability); // 300
-console.log(stormStaff2.range); // 3
+console.log(stormStaff2.name); 
+console.log(stormStaff2.attack); 
+console.log(stormStaff2.durability); 
+console.log(stormStaff2.range); 
 
 // Задача 3
 
@@ -201,20 +177,17 @@ class StudentLog {
     getName() {
         return this.name;
     }
-    addGrade(grade, subject) {   //Заполняем Журнал оценками
+    addGrade(grade, subject) {  
         if (grade >= 1 && grade <= 5) {
             switch (subject) {
                 case 'algebra':
                     algebra.push(grade);
-                    // console.log(algebra);
                     break
                 case 'geometry':
                     geometry.push(grade);
-                    // console.log(geometry);
                     break
                 case 'math':
-                    math.push(grade);
-                    // console.log(math);
+                    math.push(grade);            
                     break
             }
             return 1 + `\nЖурнал:` + `\nАлгебра ${algebra}` + `\nГеометрия ${geometry}` + `\nМатематика ${math}`
@@ -222,7 +195,7 @@ class StudentLog {
             return (`Вы пытались поставить оценку ${grade} по предмету ${subject}. Допускаются только числа от 1 до 5.\n` + 1);
         } else return (`Вы пытались поставить оценку ${grade} по предмету ${subject}. Допускаются только числа от 1 до 5.\n` + 0);
     }
-    getTotalAverage() {   //соединяем массивы оценок и расчитываем среднее
+    getTotalAverage() {   
         let sum = 0;
         let mas = algebra.concat(geometry,math);
         if (mas.length == 0) {
@@ -236,19 +209,13 @@ class StudentLog {
 }
 
 const log = new StudentLog('Олег Никифоров');
-console.log(log.getName()) // Олег Никифоров
+console.log(log.getName()) 
 console.log(log.addGrade(3, 'algebra'));
-// 1
 console.log(log.addGrade('отлично!', 'math'));
-// Вы пытались поставить оценку "отлично!" по предмету "math". Допускаются только числа от 1 до 5.
-// 0
 console.log(log.addGrade(4, 'algebra'));
-// 1
 console.log(log.addGrade(5, 'geometry'));
-// 1
 console.log(log.addGrade(25, 'geometry'));
-// Вы пытались поставить оценку "25" по предмету "geometry". Допускаются только числа от 1 до 5.
-// 1
+
 log.addGrade(2, 'algebra');
 log.addGrade(4, 'algebra');
 log.addGrade(5, 'geometry');
